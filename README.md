667eecbd2f420ba4c44ca3113df75994666022ed
Yakov <nevidomskiy8@gmail.com>
2023-04-03 09:55:30 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+To run this locally:
+
+1. run `git clone https://github.com/nevidomskii8/portfolio`
+2. Install all dependencies using `yarn` or `npm install`
+3. Start the development server using `yarn start` or `npm run start`
+4. Open up [http://localhost:8080](http://localhost:8080)
---
+++ src/components/app/app.tsx
@@ -0,0 +1,24 @@
+
+import cn from 'classnames';
+import React, {Suspense, lazy} from 'react';
+
+import {stylesContainer} from './app.module.less';
+import {stylesHeader, stylesImage, stylesLink} from './app.module.scss';
+
+const LazyStrawberryIcon = lazy(() => import('./strawberry'));
+export const App = (): React.ReactElement => (
+    <div className={stylesContainer}>
+        <div className={stylesHeader}>It works</div>
+        <Suspense fallback={'loading...'}>
+            <LazyStrawberryIcon className={stylesImage} />
+        </Suspense>
+        <div>
+            <a
+                className={cn(stylesLink)}
+                href="https://github.com/glook/webpack-typescript-react"
+                target="_blank"
+            >
+            </a>
+        </div>
+    </div>
+);
