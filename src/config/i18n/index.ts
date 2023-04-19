50ad7bcfdcbddc611e1bd7f716b1a75013bd4cdd
Yakov <nevidomskiy8@gmail.com>
2023-04-19 14:16:04 +0300

    refactor:(i18n) setup types

--- src/config/i18n/index.ts
+++ src/config/i18n/index.ts
@@ -4,21 +4,21 @@ import {initReactI18next} from 'react-i18next';
 import en from './locales/en.translation.json';
 import ua from './locales/ua.translation.json';
 
+export const defaultNS = 'ns';
+export const resources = {
+    en: {
+        ns: en,
+    },
+    ua: {
+        ns: ua,
+    },
+} as const;
+
 i18n.use(initReactI18next).init({
     lng: 'en',
-    fallbackLng: 'en',
-    resources: {
-        en: {
-            translation: en,
-        },
-        ua: {
-            translation: ua,
-        },
-    },
-    debug: false,
-    interpolation: {
-        escapeValue: false,
-    },
+    debug: true,
+    resources,
+    defaultNS,
 });
 
 export default i18n;
