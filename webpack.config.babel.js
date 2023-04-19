4172d998fd05bca0a41d68d24888fc798b9a5dbd
Jakob <49536061+nevidomskii8@users.noreply.github.com>
2023-04-20 00:42:01 +0300

    Create static.yml

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,11 @@
-
 import merge from 'webpack-merge';
 
 import baseConfig from './webpack/base';
 import devConfig from './webpack/dev';
 import prodConfig from './webpack/prod';
 import {isProd} from './webpack/utils/env';
+
+require.resolve('@babel/register');
 
 export default () =>
     isProd ? merge(baseConfig, prodConfig) : merge(baseConfig, devConfig);
